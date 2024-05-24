a4ce581ee0eda3d86b255ceb8699dd66d93b9016
AnnikDev <a_kvernadze@cu.edu.ge>
2024-05-24 12:13:03 +0400

    add main contaainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import { useState } from "react";
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <AdNum>
+          ADVICE <span>#117</span>
+        </AdNum>
+      </Container>
+    </>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 54rem;
+  height: 32rem;
+  border-radius: 1.5rem;
+  box-shadow: 3rem 5rem 8rem 0 rgba(0, 0, 0, 0.1);
+  background-color: #313a48;
+  text-align: center;
+  padding: 4.8rem 4.8rem 7.2rem;
+`;
+
+const AdNum = styled.p`
+  font-size: 1.3rem;
+  font-weight: 800;
+  letter-spacing: 0.4rem;
+  color: #53ffaa;
+`;
